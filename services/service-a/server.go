883b8dc97c4df48e57742e9f267b91a5c855ec2b
nantawat chairat <nantawat@opsta.co.th>
2026-06-30 14:42:24 +0700

    show token in home page

--- services/service-a/server.go
+++ services/service-a/server.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/base64"
 	"encoding/json"
+	"html"
 	"log"
+	"strings"
 
 	"github.com/gofiber/fiber/v3"
 )
@@ -17,26 +19,23 @@ type userinfo struct {
 
 const requiredRole = "tbox-user"
 
+const (
+	localIdentity    = "identity"
+	localIDToken     = "id_token"
+	localAccessToken = "access_token"
+)
+
 func main() {
 	app := fiber.New(fiber.Config{
 		ReadBufferSize: 1 << 20,
 	})
 
-	app.Use(requireIdentity)
-
-	app.Get("/", func(c fiber.Ctx) error {
-		ui, _ := c.Locals("userinfo").(userinfo)
-		name := ui.PreferredUsername
-		if name == "" {
-			name = "user"
-		}
-		return c.SendString("Hello, " + name + "! (role: " + requiredRole + ")")
-	})
+	app.Get("/", extractIdentity, requireRole, handler)
 
 	log.Fatal(app.Listen(":3000"))
 }
 
-func requireIdentity(c fiber.Ctx) error {
+func extractIdentity(c fiber.Ctx) error {
 	raw := c.Get("X-Userinfo")
 	if raw == "" {
 		return c.Status(fiber.StatusUnauthorized).SendString("missing identity header")
@@ -50,11 +49,58 @@ func requireIdentity(c fiber.Ctx) error {
 	if err := json.Unmarshal(decoded, &ui); err != nil {
 		return c.Status(fiber.StatusUnauthorized).SendString("invalid identity header")
 	}
+	c.Locals(localIdentity, ui)
+	c.Locals(localIDToken, c.Get("X-ID-Token"))
+	c.Locals(localAccessToken, c.Get("X-Access-Token"))
+	return c.Next()
+}
+
+func requireRole(c fiber.Ctx) error {
+	ui, ok := c.Locals(localIdentity).(userinfo)
+	if !ok {
+		return c.Status(fiber.StatusInternalServerError).SendString("identity not extracted")
+	}
 	for _, r := range ui.RealmAccess.Roles {
 		if r == requiredRole {
-			c.Locals("userinfo", ui)
 			return c.Next()
 		}
 	}
 	return c.Status(fiber.StatusForbidden).SendString("forbidden: missing role")
+}
+
+func handler(c fiber.Ctx) error {
+	ui := c.Locals(localIdentity).(userinfo)
+	idToken := c.Locals(localIDToken).(string)
+	accessToken := c.Locals(localAccessToken).(string)
+
+	name := ui.PreferredUsername
+	if name == "" {
+		name = "user"
+	}
+
+	uiJSON, _ := json.MarshalIndent(ui, "", "  ")
+
+	var b strings.Builder
+	b.WriteString("<h1>Hello, ")
+	b.WriteString(html.EscapeString(name))
+	b.WriteString("! (role: tbox-user)</h1>\n")
+	b.WriteString("<h3>X-Userinfo</h3>\n<pre>")
+	b.WriteString(html.EscapeString(string(uiJSON)))
+	b.WriteString("</pre>\n")
+	b.WriteString("<h3>X-ID-Token</h3>\n<pre>")
+	b.WriteString(html.EscapeString(truncate(idToken, 20)))
+	b.WriteString("</pre>\n")
+	b.WriteString("<h3>X-Access-Token</h3>\n<pre>")
+	b.WriteString(html.EscapeString(truncate(accessToken, 20)))
+	b.WriteString("</pre>\n")
+
+	c.Set("Content-Type", "text/html; charset=utf-8")
+	return c.SendString(b.String())
+}
+
+func truncate(s string, n int) string {
+	if len(s) > n {
+		return s[:n] + "..."
+	}
+	return s + "..."
 }
